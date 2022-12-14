fc6edf38e0e713b4a5a7fa464011d7e866a42235
crisp-k <53191886+crisp-k@users.noreply.github.com>
2022-12-14 18:18:34 -0500

    Expands Crossover Pseudocode Explanation

---
+++ gA.py
@@ -0,0 +1,96 @@
+"""
+Framework:
+Generation of new population
+    DO NOT MAKE IT MORE COMPLICATED THAN NEEDED
+
+    To wrapper function:
+        Given dimensions and number of members to generate
+        Call member generator:
+            Member generator:
+                Do:
+                    Create array of given dimensions
+                    Randomly populate each index of array with either 0 or 1
+                Return array
+
+
+Evaluation
+    For each member within the population:
+        Pass member to fitness function:
+            Fitness function (element minimization):
+                Sum the value of each array
+                Return sum
+    Pass sum and member to dtype packager:
+        Dtype packager:
+            dtype = [('member', object), ('sum', int)]
+            return np.array(member, sum, dtype=dtype)
+
+
+
+Selection
+    Pass member/sum array to sorting function
+        Sorting function:
+            Sort array of member/sum by sum
+            return array
+    
+    Elite and cull block:
+        Pass elite ratio and members to elite selector
+            Elite selector:
+                Separate top ratio of memebers from the rest of the population
+                return elite population, common population
+
+        Pass cull ratio and common population to cull function
+            Cull function:
+                Given cull ratio, remove bottom % of population
+                return resulting middle population
+
+Pairing
+    Random pairing of members for crossover
+        Pass to pairing function:
+            For each member:
+                Pair with a different, randomly picked, member
+                Package both members
+                return list of pairs
+
+    return pairs
+
+Crossover
+    Swapping of genetic information
+    3 methods of crossover will be used to start:
+        Alternate swapping of rows:
+            for each row: 
+                if row index % 2 == 0:
+                    swap rows
+
+        Alternate swapping of cols:
+            for each row:
+                if col index % 2 == 0:
+                    swap rows
+
+        Combination of both:
+            for each solution:
+                altRowSwap
+                altColSwap
+
+
+    Pass to crossover driver:
+        List of member pairs
+        Type code to indicate which crossover method to use
+
+        Call specific crossover method function based on type code
+
+    return new generation
+
+
+Mutation
+    Random chance of mutation, given by a probability
+
+    Pass new generation to mutation function:
+        For each member:
+            Step through each element of solution:
+                For each element:
+                    Flip bit based on mutation probability
+
+    return new generation
+
+---> Next iteration
+"""
