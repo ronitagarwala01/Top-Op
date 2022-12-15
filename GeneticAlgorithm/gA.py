296bc835a582056eebb18e6b0d08fa2b56d22fb0
crisp-k <53191886+crisp-k@users.noreply.github.com>
2022-12-14 23:02:09 -0500

    Population Generation

--- GeneticAlgorithm/gA.py
+++ GeneticAlgorithm/gA.py
@@ -1,3 +1,31 @@
+import numpy as np
+
+"""
+List of functions that need to be written (18 functions):
+	-Main driver function (control flow function)
+
+	-Initial population:
+		-Member generator
+
+	To be contained within a loop:
+		-Evaluation function:
+			-Fitness function
+			-Dtype packager
+		-Selection:
+			-Sorting function
+			-Elite selector
+			-Cull selector
+		Pairing:
+			-Pairing function
+		-Crossover:
+			-AlternativeRowSwap
+			-AlternativeColSwap
+			-AltRowAndColSwap
+		-Mutation:
+			-Mutator function
+			-Probability function(?)
+"""
+
 """
 Framework:
 Generation of new population
@@ -11,46 +39,110 @@ Generation of new population
                     Create array of given dimensions
                     Randomly populate each index of array with either 0 or 1
                 Return array
+"""
+
+def memberGenerator(nelx, nely):
+    member = np.random.randint(0, 2, size=(nelx, nely))
+
+    return member
+
+def generateInitalPopulation(nelx, nely, numMembers):
+    population = []
+
+    for i in range(numMembers):
+
+        population.append(memberGenerator(nelx, nely))
+
+    return population
+
+print(generateInitalPopulation(5, 5, 1))
 
 
-Evaluation
-    For each member within the population:
-        Pass member to fitness function:
-            Fitness function (element minimization):
-                Sum the value of each array
-                Return sum
-    Pass sum and member to dtype packager:
-        Dtype packager:
-            dtype = [('member', object), ('sum', int)]
-            return np.array(member, sum, dtype=dtype)
+"""
+Wrapped within an iterator:
+    Evaluation
+        For each member within the population:
+            Pass member to fitness function:
+                Fitness function (element minimization):
+                    Sum the value of each array
+                    Return sum
+        Pass sum and member to dtype packager:
+            Dtype packager:
+                dtype = [('member', object), ('sum', int)]
+                return np.array(member, sum, dtype=dtype)
+"""
+
+"""
+    Selection
+        Pass member/sum array to sorting function
+            Sorting function:
+                Sort array of member/sum by sum
+                return array
+        
+        Elite and cull block:
+            Pass elite ratio and members to elite selector
+                Elite selector:
+                    Separate top ratio of memebers from the rest of the population
+                    return elite population, common population
+
+            Pass cull ratio and common population to cull function
+                Cull function:
+                    Given cull ratio, remove bottom % of population
+                    return resulting middle population
+"""
+
+"""
+    Pairing
+        Random pairing of members for crossover
+            Pass to pairing function:
+                For each member:
+                    Pair with a different, randomly picked, member
+                    Package both members
+                    return list of pairs
+
+        return pairs
+"""
+
+"""
+    Crossover
+        Swapping of genetic information
+        3 methods of crossover will be used to start:
+            Alternate swapping of rows:
+                for each row: 
+                    if row index % 2 == 0:
+                        swap rows
+
+            Alternate swapping of cols:
+                for each row:
+                    if col index % 2 == 0:
+                        swap rows
+
+            Combination of both:
+                for each solution:
+                    altRowSwap
+                    altColSwap
 
 
+        Pass to crossover driver:
+            List of member pairs
+            Type code to indicate which crossover method to use
 
-Selection
-    Pass member/sum array to sorting function
-        Sorting function:
-            Sort array of member/sum by sum
-            return array
-    
-    Pass elite ratio and members to elite selector
-        Elite selector:
-            Separate top ratio of memebers from the rest of the population
-            return elite population, common population
+            Call specific crossover method function based on type code
 
-    Pass cull ratio and common population to cull function
-        Cull function:
-            Given cull ratio, remove bottom % of population
-            return resulting middle population
+        return new generation
+"""
 
-    Random pairing of members for crossover
+"""
+    Mutation
+        Random chance of mutation, given by a probability
 
+        Pass new generation to mutation function:
+            For each member:
+                Step through each element of solution:
+                    For each element:
+                        Flip bit based on mutation probability
 
+        return new generation
 
-Crossover
-    Swapping of genetic information
-
-
-
-Mutation
-    Random chance of mutation, given by a probability
+    ---> Next iteration
 """
