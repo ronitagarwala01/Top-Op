a4dc46c8b45ceaab426a6bab0e3f629ec4112e6d
Sam <801sly@gmail.com>
2023-01-26 12:31:38 -0500

    Added the Data
    Brought the data generated from the GA into the file. Added a small program to get the data out of the npz format and into real numpy arrays.

--- TestRandomProblemMap.py
+++ TestRandomProblemMap.py
@@ -1,10 +1,36 @@
-from ProblemMapper import *
-
+from DataCreation.ProblemMapper import *
 import matplotlib.pyplot as plt
 from matplotlib import colors
-import numpy as np
 
 
+def dispayMember(figure,figImage,member):
+    if(plt.fignum_exists(figure.number)):
+        figImage.set_array(member)
+        figure.canvas.draw()
+        figure.canvas.flush_events()
+
+def testMinArea():
+    #start by defining the topopt problem we will solve
+    nelx=30
+    nely=30
+    volfrac=0.4
+    rmin=5.4
+    penal=3.0
+    ft=0
+    maxCompliance = 30
+
+    DisplayFlag = True
+    # The variables are in order: x position of cylinder, y position of cylinder, radius of the cylinder, the magnitude of the force,
+    # and the counterclockwise angle of the force in degrees.
+    circle_1 = [.15,.15,.1,1,(3/2)*np.pi]
+    circle_2 = [.5,.5,.2,1,(1/2)*np.pi]
+    circle_3 = [.85,.85,.1,1,(3/2)*np.pi]
+
+    filledArea,supportArea,forceVector,minViableArea = mapProblemStatement2D(nelx,nely,circle_2,circle_1,circle_3,"y")
+
+    fig,ax = plt.subplots(1,1)
+    im1 = ax.imshow(minViableArea, cmap='gray_r', interpolation='none',norm=colors.Normalize(vmin=0,vmax=1))
+    plt.show()
 
 def randomCircleGenerator():
     x1 = np.random.random()
@@ -16,7 +42,7 @@ def randomCircleGenerator():
     return c1
 
 
-def main():
+def testFilledArea():
     nelx = 30
     nely = 30
 
@@ -45,4 +71,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    testFilledArea()
