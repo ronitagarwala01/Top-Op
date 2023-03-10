fae553fbb2c813131b9c5f1480117dcfa32f23c7
Sam <801sly@gmail.com>
2023-03-10 16:18:44 -0500

    Added  connecting lines function
    Added a function that takes the formatted array for the fenics input and returns a 2D array of lines connecting the center of each circle. Added bool value to it to draw circles as well as lines.

--- dataGenerationKit/problemStatementGenerator.py
+++ dataGenerationKit/problemStatementGenerator.py
@@ -238,14 +238,8 @@ def createConstraints(YoungsModulusMin,YoungsModulusMax,CmaxRatio,CminRatio,Comp
 
     return YoungsModulus,ComplianceMax,StressMax
 
-
-def testCircles():
-    c1,c2,c3 = generateRandomProblemStatement_2D(2,1)
-    print(c1[:2],c1[2])
-    print(c2[:2],c2[2])
-    print(c3[:2],c3[2])
-
-    fig, ax = plt.subplots(1,1)
+def CreateCircles(c1,c2,c3):
+    #fig, ax = plt.subplots(1,1)
     res = 500
     x= np.linspace(0,2,res)
     y = np.linspace(0,1,res//2)
@@ -260,11 +254,18 @@ def testCircles():
     Z1 = np.where(Z1<=0,-1,Z1)
 
     print(Z1.shape)
-    ax.imshow(Z1,cmap='gray')
+    return Z1
 
+def testCircles():
+    c1,c2,c3 = generateRandomProblemStatement_2D(2,1)
+    print(c1[:2],c1[2])
+    print(c2[:2],c2[2])
+    print(c3[:2],c3[2])
 
+    im = CreateCircles(c1,c2,c3)
+    fig,ax = plt.subplots(1)
+    ax.imshow(im,cmap='gray')
     plt.show()
-
 
 def testParams():
 
@@ -282,6 +283,75 @@ def testParams():
 
         print("Youngs:{:.5f}\tStress:{:.5f}\tCompliance:{:.5f}".format(y,s,c))
 
+def createConnectingLines(formatted,lineWidth:int = 2,drawCircles:bool=False):
+    """
+    Create a 2D image of lines connecting all three circles together.
+    
+    parameters:
+        - formatted: array of the initial fenics formatted array. uses only the circles part of the array
+        - lineWidth: integer representing how thick the lines should be
+        - drawCircles: bool to dictate if the circles should be draw into the array or just the lines
+    
+    returns:
+        - LinesImage: a 2d array of lines connecting the centers of the circles.
+
+    """
+
+    circles = formatted[0]
+    radii = formatted[1]
+    nelx, nely = formatted[3], formatted[4]
+    circlesArray = [[circles[0][0],circles[1][0],radii[0]],[circles[0][1],circles[1][1],radii[1]],[circles[0][2],circles[1][2],radii[2]]]
+    resolution = min(nelx,nely)
+
+    LinesImage = np.zeros((nelx,nely))
+    for i,c1 in enumerate(circlesArray):
+        for j,c2 in enumerate(circlesArray):
+            if(i==j):
+                continue
+            else:
+                c1_x = int(c1[0] * resolution)
+                c1_y = int(c1[1] * resolution)
+
+                c2_x = int(c2[0] * resolution)
+                c2_y = int(c2[1] * resolution)
+
+                yOffset = abs(c1_y - c2_y)
+                xOffset = abs(c1_x - c2_x)
+
+                if(xOffset >= yOffset):
+                    start_x = c1_x
+                    start_y = c1_y
+                    end_x = c2_x
+                    end_y = c2_y
+                    lineSlope = (end_y-start_y)/(end_x-start_x)
+                    for x in range(start_x,end_x+1):
+                        midPoint_y = lineSlope * (x-start_x) + start_y
+                        for y in range(int(np.floor(midPoint_y - lineWidth)),int(np.ceil(midPoint_y + lineWidth))):
+                            LinesImage[max(0,min(x,nelx-1)),max(0,min(y,nely-1))] = 1
+                else:
+                    start_x = c1_x
+                    start_y = c1_y
+                    end_x = c2_x
+                    end_y = c2_y
+                    #print("({}-{})/({}-{})".format(end_y,start_y,end_x,start_x))
+                    lineSlope = (end_x-start_x)/(end_y-start_y)
+                    for y in range(start_y,end_y+1):
+                        midPoint_x = lineSlope * (y-start_y) + start_x
+                        for x in range(int(np.floor(midPoint_x - lineWidth)),int(np.ceil(midPoint_x + lineWidth))):
+                            LinesImage[max(0,min(x,nelx-1)),max(0,min(y,nely-1))] = 1
+    if(drawCircles):
+    
+        for c_x,c_y,c_r in circlesArray:
+            c_x *= resolution
+            c_y *= resolution
+            c_r *= resolution
+            for x1 in range(int(np.maximum(np.floor(c_x - c_r),0)),int(np.minimum(np.ceil(c_x + c_r) + 1,nelx))):
+                for y1 in range(np.maximum(np.floor(c_y - c_r),0).astype("int32"),np.minimum(np.ceil(c_y + c_r) + 1,nely).astype("int32")):
+                    if(np.sqrt((x1-c_x)**2 + (y1-c_y)**2 ) <= c_r):
+                        LinesImage[x1,y1] = 1
+
+    
+    return LinesImage
 
 if(__name__ == "__main__"):
     testParams()
