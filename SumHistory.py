7176e023860a6546f1d453ec6e28af5ad3d626cc
Sam <801sly@gmail.com>
2023-03-22 12:23:08 -0400

    Added all history files to one file
    Created a function to sum up all history files into a single folder. Added training history image.

---
+++ SumHistory.py
@@ -0,0 +1,56 @@
+import numpy as np
+import json
+import matplotlib.pyplot as plt
+
+
+
+def plotHistory_lite(hist):
+    fig,ax = plt.subplots(1,1)
+    ax.set_title("")
+    keys = hist.keys()
+    for key in keys:
+        if('output' in key):
+            minVal = min(hist[key])
+            meanVal = np.mean(hist[key])
+            maxVal = max(hist[key])
+            if(minVal != maxVal):
+                print("{}:\n\tmin:{}\n\tmean:{}\n\tmax:{}".format(key,minVal,meanVal,maxVal))
+                #ax.plot(y,hist[key],linewidth=0.5,label=key)
+        else:
+            y = np.arange(1,len(hist[key])+1)
+            normal = 1
+            if(key == 'loss'):
+                normal = 5
+            ax.plot(y,np.array(hist[key])/normal,label=key)
+            minVal = min(hist[key])
+            meanVal = np.mean(hist[key])
+            maxVal = max(hist[key])
+            print("{}:\n\tmin:{}\n\tmean:{}\n\tmax:{}".format(key,minVal,meanVal,maxVal))
+    
+    plt.legend()#bbox_to_anchor=(1.0, 1.0), loc='upper left')
+    plt.show()
+
+def main():
+    name = 'trainHistory_'
+
+    numHistoryFolder = 20
+
+    #hist1 = json.load(open(name + str(0),'r'))
+
+    # print(hist1.keys())
+    # for i in range(1,numHistoryFolder):
+    #     newHist = json.load(open(name + str(i),'r'))
+    #     for key in hist1.keys():
+    #         for val in newHist[key]:
+    #             hist1[key].append(val)
+    
+    # print(hist1)
+    plotHistory_lite(json.load(open("Model_m9_200_epoch_trainin_run_history.hist",'r')))
+
+    #json.dump(hist1,open("Model_m9_200_epoch_trainin_run_history.hist",'w'))
+
+
+
+
+if(__name__ == "__main__"):
+    main()
