b6099259698b34e0c973412318049ed36f912af9
Sam <801sly@gmail.com>
2023-04-24 11:16:55 -0400

    Update DataForGif.py
    Added new function to test the model symmetry.

--- Iterative/DataForGif.py
+++ Iterative/DataForGif.py
@@ -110,7 +110,7 @@ def generateData(nelx, nely, dontOptimize:bool=False):
 
     
     return solutions_list, formatted, (end-start), converged
-            
+
 def iterateShiftDifferences(formated):
 
     nelx,nely = formated[3],formated[4]
@@ -207,9 +207,8 @@ def plotFormatVector(formatVector,res:int=100,name:str='formatOut'):
     plt.savefig(str(name) + ".png", format='png')
 
 
-def createSaveFolder(formatVector):
+def createSaveFolder(formatVector,agentName:str = 'Sequence'):
     nelx, nely = formatVector[3], formatVector[4]
-    agentName = 'Sequence'
 
     #Check the current path and locate the correct folder to save the data to
     workingDirectory = os.getcwd()
@@ -242,11 +241,6 @@ def createSaveFolder(formatVector):
     savedConditions = saveLoadConditions(agentFolder,formatVector)
     return agentFolder
     
-
-
-    
-
-
 def saveLoadConditions(folderToSaveTo,formattedArray):
     """
     Saves the load conditions stored inside the formatted Array
@@ -277,8 +271,6 @@ def saveLoadConditions(folderToSaveTo,formattedArray):
         print("Tried to save: {}".format(fileNameToSaveAs))
     os.chdir(originalWorkingDirectory)
     return dataIsSaved
-
-
 
 def main(size:int=100):
     nelx = size - (size%2)
@@ -354,18 +346,191 @@ def main(size:int=100):
     f.close()
 
 
+def main2():
+    nelx = 100
+    nely = nelx//2 # 50
+    _, formatedOriginal = generateProblemOrientation(nelx, nely)
+    formatedOriginal = generateProblemConditions(formatedOriginal)
+
+    formatedLR,formatedUD,formatedDiagonal = flipLoadConditions(formatedOriginal.copy())
+    formatChangeForces,formatChangeValues,formatChangeCircles = changeFormatValues(formatedOriginal.copy())
+
+    ModelPartDifferences(formatedOriginal,'Orginal')
+    ModelPartDifferences(formatChangeForces,'Forces')
+    ModelPartDifferences(formatChangeValues,'Values')
+    ModelPartDifferences(formatChangeCircles,'Circles')
+    ModelPartDifferences(formatedLR,'MirrorY')
+    ModelPartDifferences(formatedUD,'MirrorX')
+    ModelPartDifferences(formatedDiagonal,'MirrorDiagonal')
+    
+    
+def FenicsOptimizeTest(formatVector):
+    
+    print("\n\n")
+
+    print("Pass to fenics")
+    start = perf_counter()
+
+    solutions_list, objective_list, derivative_list, C_max, S_max, converged = fenicsOptimizer(formatVector)
+    formatVector[6] = C_max
+    formatVector[7] = S_max
+    
+    end = perf_counter()
+
+    
+    return solutions_list, formatVector, (end-start), converged
+
+
+def changeFormatValues(formatVector):
+    circles = formatVector[0]
+    radii = formatVector[1]
+    forces = formatVector[2]
+    nelx, nely = formatVector[3], formatVector[4]
+    Youngs, C_max, S_max = formatVector[5], formatVector[6], formatVector[7]
+
+    _, newFormat = generateProblemOrientation(nelx, nely)
+    newFormat = generateProblemConditions(newFormat)
+
+    formatChangeForces = [  circles.copy(),         radii.copy(),   newFormat[2].copy(),    nelx,nely,  Youngs, C_max,          S_max]
+    formatChangeValues = [  circles.copy(),         radii.copy(),   forces.copy(),          nelx,nely,  Youngs, newFormat[6],   newFormat[7]]
+    formatChangeCircles = [ newFormat[0].copy(),    radii.copy(),   forces.copy(),          nelx,nely,  Youngs, C_max,          S_max]
+
+    return formatChangeForces,formatChangeValues,formatChangeCircles
+
+def flipLoadConditions(formattedArray):
+    """
+    Takes the formatted Array and returns three variations with the circles fliped horizontally, vertically, and over the diagonal.
+
+    Parameters:
+        - formattedArray
+    
+    returns:
+        - formatted LR: fliped left right(horizontal mirror)
+        - formatted UD: fliped up down(vertical mirror)
+        - formatted diagnal: fliped over both horizontal and vertical
+    """
+    circles = formattedArray[0]
+    radii = formattedArray[1]
+    forces = formattedArray[2]
+    nelx, nely = formattedArray[3], formattedArray[4]
+    Y, C_max, S_max = formattedArray[5], formattedArray[6], formattedArray[7]
+
+    #create arrays for circle positions
+    circlesLR = circles.copy()
+    circlesUD = circles.copy()
+    circlesDiagonal = circles.copy()
+
+    #flip the x positions
+    circlesLR[0,:] = 2-circlesLR[0,:]
+
+    #flip y postions
+    circlesUD[1,:] = 1-circlesUD[1,:]
+
+    #flip diagonal
+    circlesDiagonal[0,:] = circlesLR[0,:]
+    circlesDiagonal[1,:] = circlesUD[1,:]
+
+    forcesLR = forces.copy()
+    forcesUD = forces.copy()
+    forcesDiagonal = forces.copy()
+
+    #flip the x magnitude
+    forcesLR[0,:] = -forcesLR[0,:]
+
+    #flip y magnitude
+    forcesUD[1,:] = -forcesUD[1,:]
+
+    #flip diagonal
+    forcesDiagonal[0,:] = forcesLR[0,:]
+    forcesDiagonal[1,:] = forcesUD[1,:]
+
+    #duplicate
+    formattedLR = [circlesLR,radii,forcesLR,nelx,nely,Y,C_max,S_max]
+    formattedUD = [circlesUD,radii,forcesUD,nelx,nely,Y,C_max,S_max]
+    formattedDiagonal = [circlesDiagonal,radii,forcesDiagonal,nelx,nely,Y,C_max,S_max]
+
+    return formattedLR,formattedUD,formattedDiagonal
+
+def ModelPartDifferences(formatVector,trial:str='Sequence'):
+    circles = formatVector[0]
+    radii = formatVector[1]
+    forces = formatVector[2]
+    nelx, nely = formatVector[3], formatVector[4]
+    Youngs, C_max, S_max = formatVector[5], formatVector[6], formatVector[7]
+
+    solutions_list, formatted, fenicsTime, converged = FenicsOptimizeTest(formatVector)
+    print(formatted)
+    predictions_list,modelTime = iterateShiftDifferences(formatted)
+
+    solution_asImages = []
+    for i in range(len(solutions_list)):
+
+        solution_asImages.append(np.reshape(solutions_list[i],(nelx+1,nely+1),order='F'))
+
+    agentFolderPath = createSaveFolder(formatted,trial)
+
+    cwd = os.getcwd()
+    os.chdir(agentFolderPath)
+    plotFormatVector(formatted)
+    SaveAsGif(solution_asImages,nelx,nely,"FenicsOutput")
+    SaveAsGif(predictions_list,nelx,nely,"ModelOutput")
+    #json.dump(saveStatsForEachIteration(predictions_list,formatted),open(os.path.join(agentFolderPath,"modelStatsOverIteration.json"),'w'))
+    os.chdir(cwd)
+
+    print("\n")
+    print("Fenics took {:.2f} seconds.\nModel took {:.2f} seconds.\n".format(fenicsTime,modelTime))
+
+    #print("\tCompliance Max: {}".format(c_max))
+    #print("\tStress Max: {}".format(s_max))
+
+    
+    part_flat = np.ravel(solution_asImages[-1],order='F')
+
+    #print("\tCompliance Max: {}".format(c_max))
+    #print("\tStress Max: {}".format(s_max))
+
+    compliance,stress = convergenceTester(formatted,part_flat,1)
+    mass = np.sum(part_flat)
+    f = open(os.path.join(agentFolderPath,"ModelComparison.txt"),'w')
+
+    f.write("Circle 1: ( {:.2f}, {:.2f} ) radius = {:.3f}\n".format(formatted[0][0][0],formatted[0][1][0],formatted[1][0]))
+    f.write("Circle 2: ( {:.2f}, {:.2f} ) radius = {:.3f}\n".format(formatted[0][0][1],formatted[0][1][1],formatted[1][1]))
+    f.write("Circle 3: ( {:.2f}, {:.2f} ) radius = {:.3f}\n".format(formatted[0][0][2],formatted[0][1][2],formatted[1][2]))
+
+    f.write("\nForce 1: ( {:.2e}, {:.2e} ) magnitued = {:.3e}\n".format(formatted[2][0][0],formatted[2][1][0],np.sqrt(formatted[2][0][0]**2 + formatted[2][1][0]**2)))
+    f.write("Force 2: ( {:.2e}, {:.2e} ) magnitued = {:.3e}\n".format(formatted[2][0][1],formatted[2][1][1],np.sqrt(formatted[2][0][1]**2 + formatted[2][1][1]**2)))
+    f.write("Force 3: ( {:.2e}, {:.2e} ) magnitued = {:.3e}\n".format(formatted[2][0][2],formatted[2][1][2],np.sqrt(formatted[2][0][2]**2 + formatted[2][1][2]**2)))
+
+    f.write("\nYoung's Modulus: {:.5e}\n".format(formatted[5]))
+    f.write("\nCompliance: {:.5f}\n".format(formatted[6]))
+    f.write("\nStress: {:.5e}\n".format(formatted[7]))
+
+
+    f.write("\nFenics part:\n")
+    f.write("\tmass: {}\n".format(int(mass)))
+    f.write("\tCompliance: {:.5f}\n".format(compliance))
+    f.write("\tStress: {:.5e}\n".format(stress))
+
+    part_flat = np.ravel(predictions_list[-1],order='F')
+    compliance,stress = convergenceTester(formatted,part_flat,1)
+    mass = np.sum(part_flat)
+    f.write("\nModel part:\n")
+    f.write("\tmass: {}\n".format(int(mass)))
+    f.write("\tCompliance: {:.5f}\n".format(compliance))
+    f.write("\tStress: {:.5e}\n".format(stress))
+
+    f.write("\nFenics took {:.2f} seconds.\nModel took {:.2f} seconds.\n".format(fenicsTime,modelTime))
+    if(converged):
+        f.write("Fenics part converged.")
+    else:
+        f.write("Fenics part did not converge.")
+
+    f.close()
 
 
 if(__name__ == "__main__"):
-    possible_nelx_values =  np.array([80,100,200])
-    weightsForValues =      np.array([ 4, 15,  1])
 
-    norm = weightsForValues/np.sum(weightsForValues)
-
-    partsToGenerate = int(2*np.sum(weightsForValues))
-    #print(partsToGenerate)
-    for i in range(partsToGenerate):
-        resolution = np.random.choice(possible_nelx_values,p=norm)
-        print("part: {} of {}, resolution= {}.".format(i,partsToGenerate,resolution))
-        main(resolution)    
+    
+        
+    main2()    
 
